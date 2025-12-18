f520544eb6f91bff60c4177467ffd1cc382c7f32
rayhantmt <rayhantmt@gmail.com>
2025-12-19 03:27:52 +0600

    splash screen done

--- lib/modules/splash_screen/splash_screen.dart
+++ lib/modules/splash_screen/splash_screen.dart
@@ -1,18 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rai/utils/app_images.dart';
+import 'package:rai/utils/app_pages.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(milliseconds: 1500)).then((_) {
+      Get.toNamed(AppPages.login);
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(child: Image.asset(AppImages.logo,
-      height: Get.height*0.2,
-      width: Get.width*0.7,
-      ),),
+      body: Center(
+        child: Image.asset(
+          AppImages.logo,
+          height: Get.height * 0.2,
+          width: Get.width * 0.7,
+        ),
+      ),
     );
   }
 }

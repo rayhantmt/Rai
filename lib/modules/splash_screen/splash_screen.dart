b03727f1d0757af365081352e8c707196a94476f
rayhantmt <rayhantmt@gmail.com>
2025-12-19 02:42:12 +0600

    done with the splash screen

--- lib/modules/splash_screen/splash_screen.dart
+++ lib/modules/splash_screen/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:rai/utils/app_images.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -7,6 +9,10 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      body: Center(child: Image.asset(AppImages.logo,
+      height: Get.height*0.2,
+      width: Get.width*0.7,
+      ),),
     );
   }
 }

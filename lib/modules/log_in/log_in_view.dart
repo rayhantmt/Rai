9565a1f9533361eb18f842b0b31bf90d9482f4b5
rayhantmt <rayhantmt@gmail.com>
2025-12-19 04:06:58 +0600

    done desinging

--- lib/modules/log_in/log_in_view.dart
+++ lib/modules/log_in/log_in_view.dart
@@ -11,18 +11,23 @@ class LogInView extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xff19191B),
       body: Padding(
-        padding: const EdgeInsets.only(left: 20,right: 20),
+        padding: const EdgeInsets.only(left: 20, right: 20),
         child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: Get.height*0.1,),
-              Image.asset(AppImages.logo,
-              height: Get.height*0.1,
-              width: Get.width*0.3,
+              SizedBox(height: Get.height * 0.1),
+              Image.asset(
+                AppImages.logo,
+                height: Get.height * 0.1,
+                width: Get.width * 0.3,
               ),
-              Commontextfield(tittle: 'Username or Email/phone number', hint: 'Enter username or email/phone number', obsecuretext: false)
+              Commontextfield(
+                tittle: 'Username or Email/phone number',
+                hint: 'Enter username or email/phone number',
+                obsecuretext: false,
+              ),
             ],
           ),
         ),

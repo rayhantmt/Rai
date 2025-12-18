a86ca971c6a52f69452c30f5fbfa2153b3dcba2e
rayhantmt <rayhantmt@gmail.com>
2025-12-19 03:40:58 +0600

    creating the common textfield

---
+++ lib/common_widgets/common_textfield.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CommonTextfield extends StatelessWidget {
+  const CommonTextfield({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}

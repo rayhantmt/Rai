35966434f536bf1f037106c4090d58dff3c3cbfd
rayhantmt <rayhantmt@gmail.com>
2025-12-19 03:43:35 +0600

    added google fonts and applied

--- lib/common_widgets/common_textfield.dart
+++ lib/common_widgets/common_textfield.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class CommonTextfield extends StatelessWidget {
-  const CommonTextfield({super.key});
-
+class Commontextfield extends StatelessWidget {
+  const Commontextfield({super.key, required this.tittle, required this.hint,required this.obsecuretext,this.suffix});
+final String tittle;
+final String hint;
+final bool obsecuretext;
+final Widget? suffix;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(tittle,
+        style: GoogleFonts.roboto( fontWeight: FontWeight.w600,fontSize: 14,color: Colors.white),
+        ),
+        Container(
+          height: Get.height*0.04,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.white.withOpacity(0.8)
+            ),
+            borderRadius: BorderRadius.circular(12)
+          ),
+          child: TextFormField(
+          style: GoogleFonts.inter( color: Colors.white),
+            obscureText: obsecuretext,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              suffix: suffix,
+              hint: Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Text(hint,
+                style: GoogleFonts.roboto( fontWeight: FontWeight.w400,fontSize: 12,
+                color: Colors.white.withOpacity(0.6)
+                ),
+                ),
+                
+              )
+            ),
+          ),
+        ),
+      ],
     );
   }
 }

5d05dd10a75c5b114dcace50c10b91f8920471e9
rayhantmt <rayhantmt@gmail.com>
2025-12-20 19:37:37 +0600

    added the first page in routing

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 import 'package:rai/modules/log_in/log_in_binding.dart';
 import 'package:rai/modules/log_in/log_in_view.dart';
+import 'package:rai/modules/sign_up/1st_page/1st_page.dart';
 import 'package:rai/utils/app_pages.dart';
 
 class AppRoutes {
  static var getpages=[
-    GetPage(name: AppPages.login, page: () => LogInView(),binding: LogInBinding())
+    GetPage(name: AppPages.login, page: () => LogInView(),binding: LogInBinding()),
+    GetPage(name: AppPages.page, page: () => Firstpage(),)
   ];
 }

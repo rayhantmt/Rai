d9ee3d60ab6d4d3784ecc594e2ec9b99b0764ed0
rayhantmt <rayhantmt@gmail.com>
2025-12-20 19:32:09 +0600

    designing the 1st page of sign up

---
+++ lib/modules/sign_up/1st_page/1st_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Firstpage extends StatelessWidget {
+  const Firstpage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff19191B),
+    );
+  }
+}

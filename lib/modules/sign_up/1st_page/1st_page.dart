3b369b04f4ebde487627c5777b77a4acf21ddaa4
rayhantmt <rayhantmt@gmail.com>
2025-12-20 19:36:08 +0600

    added the 18 text

--- lib/modules/sign_up/1st_page/1st_page.dart
+++ lib/modules/sign_up/1st_page/1st_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Firstpage extends StatelessWidget {
   const Firstpage({super.key});
@@ -7,6 +9,21 @@ class Firstpage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff19191B),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20,right: 20),
+        child: Column(
+          children: [
+            SizedBox(height: Get.height*0.1,),
+            Text('Are You 18 or Older?',
+            style: GoogleFonts.inter( 
+              fontWeight: FontWeight.w700,
+              fontSize: 24,
+              color: Colors.white
+            ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }

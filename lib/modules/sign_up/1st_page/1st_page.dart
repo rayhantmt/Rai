b0069517a29f3a699ec8b390b34507261812fc41
rayhantmt <rayhantmt@gmail.com>
2025-12-20 19:41:40 +0600

    added the texts

--- lib/modules/sign_up/1st_page/1st_page.dart
+++ lib/modules/sign_up/1st_page/1st_page.dart
@@ -12,13 +12,22 @@ class Firstpage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.only(left: 20,right: 20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: Get.height*0.1,),
+            SizedBox(height: Get.height*0.15,),
             Text('Are You 18 or Older?',
             style: GoogleFonts.inter( 
               fontWeight: FontWeight.w700,
               fontSize: 24,
               color: Colors.white
+            ),
+            ),
+            SizedBox(height: Get.height*0.02,),
+            Text('This app is only available to users 18 years or older.',
+            style: GoogleFonts.inter( 
+              fontWeight: FontWeight.w500,
+              fontSize: 16,
+              color: Color(0xffB2B3BD)
             ),
             )
           ],

46217a0f065e0e140c741bf6328bd8cbd4a27a80
rayhantmt <rayhantmt@gmail.com>
2025-12-21 22:26:50 +0600

    added the page in app routing

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -2,11 +2,13 @@ import 'package:get/get.dart';
 import 'package:rai/modules/log_in/log_in_binding.dart';
 import 'package:rai/modules/log_in/log_in_view.dart';
 import 'package:rai/modules/sign_up/1st_page/1st_page.dart';
+import 'package:rai/modules/sign_up/2nd_page/2nd_page_view.dart';
 import 'package:rai/utils/app_pages.dart';
 
 class AppRoutes {
  static var getpages=[
     GetPage(name: AppPages.login, page: () => LogInView(),binding: LogInBinding()),
-    GetPage(name: AppPages.page, page: () => Firstpage(),)
+    GetPage(name: AppPages.page, page: () => Firstpage(),),
+    GetPage(name: AppPages.secondpage, page:  () => SecondPageView(),)
   ];
 }

eb1b29584acca1afef3f031ce618286b186095ce
rayhantmt <rayhantmt@gmail.com>
2025-12-21 22:20:51 +0600

    created the skeleton of second page

---
+++ lib/modules/sign_up/2nd_page/2nd_page_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SecondPageView extends StatelessWidget {
+  const SecondPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

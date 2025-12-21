6e25bef27fdf7746d284341c9916c3d0b8135596
rayhantmt <rayhantmt@gmail.com>
2025-12-21 22:55:01 +0600

    added the text

--- lib/modules/sign_up/2nd_page/2nd_page_view.dart
+++ lib/modules/sign_up/2nd_page/2nd_page_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rai/utils/app_images.dart';
 
 class SecondPageView extends StatelessWidget {
@@ -9,12 +10,22 @@ class SecondPageView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: Get.height*0.1,),
-            Text('Create an account!')
-          ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: Get.height * 0.1),
+              Text(
+                'Create an account!',
+                style: GoogleFonts.manrope(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

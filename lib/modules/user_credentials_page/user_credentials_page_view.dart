6d6d2228690af75536b280d7ea6814f7af85e7f9
rayhantmt <rayhantmt@gmail.com>
2025-12-22 22:51:27 +0600

    starting new screen

---
+++ lib/modules/user_credentials_page/user_credentials_page_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UserCredentialsPageView extends StatelessWidget {
+  const UserCredentialsPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}

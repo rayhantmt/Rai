a507c15ccef6c94c1e34f2c4e142513f5a9e7973
rayhantmt <rayhantmt@gmail.com>
2025-12-23 02:43:03 +0600

    working on set profile image screen

---
+++ lib/modules/set_profile/set_profile_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:rai/utils/app_images.dart';
+
+class SetProfileView extends StatelessWidget {
+  const SetProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppImages.primarycolor,
+    );
+  }
+}

b05e6dcd7f292680cd4b24b6d1384eb9e250811f
rayhantmt <rayhantmt@gmail.com>
2025-12-23 02:36:09 +0600

    added the confirm password field

--- lib/modules/user_credentials_page/user_credentials_page_view.dart
+++ lib/modules/user_credentials_page/user_credentials_page_view.dart
@@ -18,35 +18,71 @@ class UserCredentialsPageView extends GetView<UserCredentialsPageController> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: Get.height*0.1,),
-              Text('User Credential!',
-              style: GoogleFonts.manrope( 
-                fontWeight: FontWeight.w600,
-                fontSize: 24,
-                color: Colors.white
-              ),
-              ),
-              SizedBox(height: Get.height*0.02,),
-              Text('Set an username &  strong password to secure account',
-              style: GoogleFonts.manrope( 
-                fontWeight: FontWeight.w400,
-                fontSize: 14,
-                color: Color(0xffB2B3BD)
-              ),
-              ),
-              SizedBox(height: Get.height*0.03,),
-              Commontextfield(tittle: 'Username', hint: 'Enter unique name', obsecuretext: false),
-              SizedBox(height: Get.height*0.02,),
-              Obx(() => Commontextfield(tittle: 'Password', hint: 'Enter a strong pawword', obsecuretext: controller.isobsecured.value,
-              suffix: GestureDetector(
-                onTap: () => controller.toggle1(),
-                child: Padding(
-                  padding: const EdgeInsets.only(right: 20),
-                  child: Icon(controller.isobsecured.value?Icons.visibility:Icons.visibility_off,color: Color(0xff5F606A),),
+              SizedBox(height: Get.height * 0.1),
+              Text(
+                'User Credential!',
+                style: GoogleFonts.manrope(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 24,
+                  color: Colors.white,
                 ),
               ),
-              ),)
+              SizedBox(height: Get.height * 0.02),
+              Text(
+                'Set an username &  strong password to secure account',
+                style: GoogleFonts.manrope(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 14,
+                  color: Color(0xffB2B3BD),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.03),
+              Commontextfield(
+                tittle: 'Username',
+                hint: 'Enter unique name',
+                obsecuretext: false,
+              ),
+              SizedBox(height: Get.height * 0.02),
+              Obx(
+                () => Commontextfield(
+                  tittle: 'Password',
+                  hint: 'Enter a strong pawword',
+                  obsecuretext: controller.isobsecured.value,
+                  suffix: GestureDetector(
+                    onTap: () => controller.toggle1(),
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: Icon(
+                        controller.isobsecured.value
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Color(0xff5F606A),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.02),
 
+              Obx(
+                () => Commontextfield(
+                  tittle: 'Confirm Password',
+                  hint: 'Enter a strong pawword',
+                  obsecuretext: controller.isobsecured.value,
+                  suffix: GestureDetector(
+                    onTap: () => controller.toggle1(),
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: Icon(
+                        controller.isobsecured.value
+                            ? Icons.visibility
+                            : Icons.visibility_off,
+                        color: Color(0xff5F606A),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),

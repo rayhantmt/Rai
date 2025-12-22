4d6808de33c3629dda813525d7344778ee90e0e8
rayhantmt <rayhantmt@gmail.com>
2025-12-23 02:49:02 +0600

    added the app bar

--- lib/modules/set_profile/set_profile_view.dart
+++ lib/modules/set_profile/set_profile_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rai/utils/app_images.dart';
 
 class SetProfileView extends StatelessWidget {
@@ -8,6 +10,20 @@ class SetProfileView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: Get.height*0.1,),
+            Text('Set up your profile!',
+            style: GoogleFonts.manrope( 
+              fontWeight: FontWeight.w600,
+              fontSize: 24,
+              color: Colors.white
+            ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }

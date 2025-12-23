9a66c6571aca5ceeb243a1421102f6e86f6f1561
rayhantmt <rayhantmt@gmail.com>
2025-12-24 00:22:34 +0600

    added the set password page

---
+++ lib/modules/fotgot_password/set_password_page/set_password_page_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:rai/utils/app_images.dart';
+
+class SetPasswordPageView extends StatelessWidget {
+  const SetPasswordPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppImages.primarycolor,
+    );
+  }
+}

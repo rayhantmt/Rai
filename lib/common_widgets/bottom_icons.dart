5df9b792e08cf518a5de71a5c5a1a5e1668618f2
rayhantmt <rayhantmt@gmail.com>
2026-01-11 06:11:10 +0600

    making the coomon bottom icons

---
+++ lib/common_widgets/bottom_icons.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BottomIcons extends StatelessWidget {
+  final String tittle;
+  final String img;
+  const BottomIcons({super.key, required this.tittle, required this.img});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+
+      ],
+    );
+  }
+}
---
+++ lib/modules/main_screen/main_screen_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MainScreenView extends StatelessWidget {
+  const MainScreenView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(child: 
+          Container(
+            height: Get.height*0.75,
+            width: double.infinity,
+            child: Row(
+              children: [
+                
+              ],
+            ),
+          )
+          )
+        ],
+      ),
+    );
+  }
+}

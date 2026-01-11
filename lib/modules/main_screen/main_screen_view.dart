f248767358f8c0260cd34ddfd3553e1a9f810128
rayhantmt <rayhantmt@gmail.com>
2026-01-11 06:19:29 +0600

    added the bottom container

--- lib/modules/main_screen/main_screen_view.dart
+++ lib/modules/main_screen/main_screen_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:rai/utils/app_images.dart';
 
 class MainScreenView extends StatelessWidget {
   const MainScreenView({super.key});
@@ -9,17 +10,27 @@ class MainScreenView extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(child: 
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: 
           Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 0.5,
+                color: Color(0xff6C6E79)
+              )
+            ),
             height: Get.height*0.75,
             width: double.infinity,
             child: Row(
               children: [
                 Column(
                   children: [
-                    Image.asset(''),
-                    Text(''),
-                    
+                    Image.asset(AppImages.homeicon),
+                    Text('Home'),
+
                   ],
                 )
               ],

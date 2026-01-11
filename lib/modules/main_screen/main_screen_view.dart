18b0a0581508caf5260c6c990bb7b1d1b0c2ebdc
rayhantmt <rayhantmt@gmail.com>
2026-01-11 06:20:49 +0600

    added the main screen

--- lib/modules/main_screen/main_screen_view.dart
+++ lib/modules/main_screen/main_screen_view.dart
@@ -14,29 +14,25 @@ class MainScreenView extends StatelessWidget {
             bottom: 0,
             left: 0,
             right: 0,
-            child: 
-          Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 0.5,
-                color: Color(0xff6C6E79)
-              )
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(32),
+                  topRight: Radius.circular(32),
+                ),
+                border: Border.all(width: 0.5, color: Color(0xff6C6E79)),
+              ),
+              height: Get.height * 0.75,
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Column(
+                    children: [Image.asset(AppImages.homeicon), Text('Home')],
+                  ),
+                ],
+              ),
             ),
-            height: Get.height*0.75,
-            width: double.infinity,
-            child: Row(
-              children: [
-                Column(
-                  children: [
-                    Image.asset(AppImages.homeicon),
-                    Text('Home'),
-
-                  ],
-                )
-              ],
-            ),
-          )
-          )
+          ),
         ],
       ),
     );

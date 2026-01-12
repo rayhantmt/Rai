00caaa485dbc03bf24a69fedbbec5fb1c5ec7bfa
rayhantmt <rayhantmt@gmail.com>
2026-01-13 00:49:26 +0600

    formatted the document

--- lib/modules/community/community_binding.dart
+++ lib/modules/community/community_binding.dart
@@ -1,10 +1,9 @@
 import 'package:get/get.dart';
-import 'package:get/get_instance/get_instance.dart';
 import 'package:rai/modules/community/community_controller.dart';
 
-class CommunityBinding extends Bindings{
+class CommunityBinding extends Bindings {
   @override
   void dependencies() {
-   Get.lazyPut<CommunityController>(()=>CommunityController());
+    Get.lazyPut<CommunityController>(() => CommunityController());
   }
 }

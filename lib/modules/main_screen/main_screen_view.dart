b220ad42cbb4f2618b6d7370f58a70a0761df0b7
rayhantmt <rayhantmt@gmail.com>
2026-01-13 00:11:51 +0600

    done adding the navbar condition

--- lib/modules/main_screen/main_screen_view.dart
+++ lib/modules/main_screen/main_screen_view.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rai/modules/home/home_view.dart';
+import 'package:rai/modules/main_screen/main_screen_controller.dart';
 import 'package:rai/utils/app_images.dart';
 
-class MainScreenView extends StatelessWidget {
+class MainScreenView extends GetView<MainScreenController> {
   const MainScreenView({super.key});
 
   @override
@@ -17,7 +19,7 @@ class MainScreenView extends StatelessWidget {
             bottom: 0,
             left: 0,
             right: 0,
-            child: Container(
+            child: Obx(()=>Container(
               decoration: BoxDecoration(
                 color: Color(0xff19191B),
                 borderRadius: BorderRadius.only(
@@ -31,53 +33,74 @@ class MainScreenView extends StatelessWidget {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Column(
-                    children: [
-                      Image.asset(
-                        AppImages.communityicon,
-                        height: Get.height * 0.035,
-                        
-                      ),
-                      Text('Home'),
-                    ],
+                  GestureDetector(
+                    onTap: () => controller.setpagetype(0),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          AppImages.communityicon,
+                          height: Get.height * 0.035,
+                          
+                        ),
+                        Text('Community'),
+                      ],
+                    ),
                   ),
-                  Column(
-                    children: [
-                      Image.asset(
-                        AppImages.aiicon,
-                        color: Colors.white,
-                        height: Get.height * 0.035,
-                      ),
-                      Text('Home'),
-                    ],
-                  ),Column(
-                    children: [
-                      Image.asset(
-                        AppImages.homeicon,
-                        height: Get.height * 0.035,
-                      ),
-                      Text('Home'),
-                    ],
-                  ),Column(
-                    children: [
-                      Image.asset(
-                        AppImages.picksicon,
-                        height: Get.height * 0.035,
-                      ),
-                      Text('Home'),
-                    ],
-                  ),Column(
-                    children: [
-                      Image.asset(
-                        AppImages.moreicon,
-                        height: Get.height * 0.035,
-                      ),
-                      Text('Home'),
-                    ],
+                  GestureDetector(
+                    onTap: () => controller.setpagetype(1),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          AppImages.aiicon,
+                          color: Colors.white,
+                          height: Get.height * 0.035,
+                        ),
+                        Text('rai. Chat'),
+                      ],
+                    ),
+                  ),GestureDetector(
+                    onTap: () => controller.setpagetype(2),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          AppImages.homeicon,
+                          height: Get.height * 0.035,
+                        ),
+                        Text('Home'),
+                      ],
+                    ),
+                  ),GestureDetector(
+                    onTap: () => controller.setpagetype(3),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          AppImages.picksicon,
+                          height: Get.height * 0.035,
+                        ),
+                        Text("rai's picks"),
+                      ],
+                    ),
+                  ),GestureDetector(
+                    onTap: () => controller.setpagetype(4),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          AppImages.moreicon,
+                          height: Get.height * 0.035,
+                          color: controller.pagetype.value==4?Color(0xffEEEEF0): Color(0xff6C6E79),
+                        ),
+                        Text('More',
+                        style:GoogleFonts.manrope( 
+                          fontWeight: FontWeight.w500,
+color: controller.pagetype.value==4?Color(0xffEEEEF0): Color(0xff6C6E79),
+                        ) ,
+                        ),
+                      ],
+                    ),
                   ),
                 ],
               ),
-            ),
+            ),)
           ),
         ],
       ),

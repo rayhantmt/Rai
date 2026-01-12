1650ae7e90d54b300d07ce7dfc88d342dfb6acdf
rayhantmt <rayhantmt@gmail.com>
2026-01-13 02:28:20 +0600

    added teh

--- lib/modules/rai_chat/rai_chat_view.dart
+++ lib/modules/rai_chat/rai_chat_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:rai/utils/app_images.dart';
 
 class RaiChatView extends StatelessWidget {
   const RaiChatView({super.key});
@@ -6,9 +8,20 @@ class RaiChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Raichat'),
-      ),
+      backgroundColor: AppImages.primarycolor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: Column(
+          children: [
+            SizedBox(height: Get.height*0.05,),
+            Row(
+              children: [
+                Image.asset(AppImages.aiicon)
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }

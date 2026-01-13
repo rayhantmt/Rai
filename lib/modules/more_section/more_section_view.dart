1c2fb7e31bcbb6b7cffc4dd21b9a82dfe42da268
rayhantmt <rayhantmt@gmail.com>
2026-01-14 04:43:39 +0600

    working on the more screen

--- lib/modules/more_section/more_section_view.dart
+++ lib/modules/more_section/more_section_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:rai/utils/app_images.dart';
 
 class MoreSectionView extends StatelessWidget {
   const MoreSectionView({super.key});
@@ -6,8 +9,24 @@ class MoreSectionView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('More section'),
+      backgroundColor: AppImages.primarycolor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: Get.height * 0.07),
+
+              Text('More Tools',
+              style: GoogleFonts.manrope( 
+                fontWeight: FontWeight.w700,
+                fontSize: 22,
+                color: Color(0xffEEEEF0)
+              ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

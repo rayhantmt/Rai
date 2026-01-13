36199232b5fe14a8e46edec5791873667c948f04
rayhantmt <rayhantmt@gmail.com>
2026-01-14 04:56:10 +0600

    added the app bar

--- lib/modules/more_section/more_section_view.dart
+++ lib/modules/more_section/more_section_view.dart
@@ -14,6 +14,7 @@ class MoreSectionView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(height: Get.height * 0.07),
 
@@ -23,6 +24,42 @@ class MoreSectionView extends StatelessWidget {
                   fontWeight: FontWeight.w700,
                   fontSize: 22,
                   color: Color(0xffEEEEF0),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.02),
+              Container(
+                height: Get.height * 0.08,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Color(0xff222325),
+                ),
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage: AssetImage(AppImages.profileimg),
+                    ),
+                    SizedBox(width: Get.width*0.05,),
+                    Column(
+                      children: [
+                        Text('Adam smith',
+                        style: GoogleFonts.manrope( 
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16,
+                          color: Color(0xffEEEEF0)
+                        ),
+                        ),
+                        Text('new@123',
+                        style: GoogleFonts.manrope( 
+                          fontWeight: FontWeight.w500,
+                          fontSize: 14,
+                          color: Color(0xffB2B3BD)
+                        ),
+                        )
+                      ],
+                    )
+                  ],
                 ),
               ),
             ],

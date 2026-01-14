64b20c441b6d7259e80fb488a53c4132fb7d728e
rayhantmt <rayhantmt@gmail.com>
2026-01-14 22:59:05 +0600

    created the common component

--- lib/common_widgets/more_section_widget.dart
+++ lib/common_widgets/more_section_widget.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MoreSectionWidget extends StatelessWidget {
-  const MoreSectionWidget({super.key});
+  final String tittle;
+  final String img;
+  const MoreSectionWidget({super.key, required this.tittle, required this.img});
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        
+Image.asset(img),
+Text(tittle,
+style: GoogleFonts.manrope( 
+  fontWeight: FontWeight.w500,
+  fontSize: 14,
+  color: Color(0xffEEEEF0)
+),
+)
       ],
     );
   }

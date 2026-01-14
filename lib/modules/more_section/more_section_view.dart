c43448293787cd2cd8557759a7c49557fa39653c
rayhantmt <rayhantmt@gmail.com>
2026-01-15 00:13:50 +0600

    imge error

--- lib/modules/more_section/more_section_view.dart
+++ lib/modules/more_section/more_section_view.dart
@@ -10,7 +10,7 @@ class MoreSectionView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppImages.primarycolor,
+     // backgroundColor: AppImages.primarycolor,
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
         child: SingleChildScrollView(

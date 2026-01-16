7f33f89961a9d6303bf7ed3d6b3e26f4ff597846
rayhantmt <rayhantmt@gmail.com>
2026-01-17 01:56:10 +0600

    added the whole design

--- lib/modules/comment_concern/comment_concern_view.dart
+++ lib/modules/comment_concern/comment_concern_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rai/utils/app_images.dart';
 
 class CommentConcernView extends StatelessWidget {
@@ -10,17 +11,41 @@ class CommentConcernView extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: Get.height*0.1,
-            ),
-            Row(
-              children: [
-                Image.asset(AppImages.commentconcernbackicon)
-              ],
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: Get.height*0.07,
+              ),
+              Row(
+                children: [
+                  Image.asset(AppImages.commentconcernbackicon,
+                  height: Get.height*0.05,
+                  
+                  ),
+
+                  SizedBox(width: Get.width*0.05,),
+                  Text('Comment & Concern',
+                  style: GoogleFonts.manrope( 
+                    fontWeight: FontWeight.w700,
+                    fontSize: 22,
+                    color: Color(0xffEEEEF0)
+                  ),
+                  )
+                ],
+              ),
+              Center(
+                child: Text('No comment & concern found',
+                style: GoogleFonts.manrope( 
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                  color: Color(0xffB2B3BD)
+                ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );

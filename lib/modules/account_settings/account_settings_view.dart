328439afa50e69d9a06a0c210bc5845193b47ea8
rayhantmt <rayhantmt@gmail.com>
2026-01-17 22:57:44 +0600

    done with the app bar

--- lib/modules/account_settings/account_settings_view.dart
+++ lib/modules/account_settings/account_settings_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rai/utils/app_images.dart';
 
 class AccountSettingsView extends StatelessWidget {
@@ -8,6 +10,33 @@ class AccountSettingsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                height: Get.height*0.07,
+              ),
+              Row(
+                children: [
+                  Image.asset(AppImages.commentconcernbackicon,
+                  height: Get.height*0.05,
+                  ),
+                  SizedBox(width: Get.width*0.05,),
+                  Text('Account Setting',
+                  style: GoogleFonts.manrope( 
+                    fontWeight: FontWeight.w700,
+                    fontSize: 22,
+                    color: Color(0xffEEEEF0)
+                  ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }

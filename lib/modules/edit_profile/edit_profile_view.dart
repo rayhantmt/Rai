abd9e1ed247fe6f43518c0bc667873ff421fe265
rayhantmt <rayhantmt@gmail.com>
2026-01-18 02:03:32 +0600

    starting the edit profile

---
+++ lib/modules/edit_profile/edit_profile_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:rai/utils/app_images.dart';
+
+class EditProfileView extends StatelessWidget {
+  const EditProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppImages.primarycolor,
+    );
+  }
+}

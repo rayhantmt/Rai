9e0d89223901dee31c7324671ebb81c7745d7f73
rayhantmt <rayhantmt@gmail.com>
2026-01-23 05:25:17 +0600

    added padding

--- lib/modules/community/community_view.dart
+++ lib/modules/community/community_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:get/state_manager.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rai/utils/app_images.dart';
 
 class CommunityView extends GetView {
@@ -9,12 +11,30 @@ class CommunityView extends GetView {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
-      body: Center(
-        child: Text('Community',
-        style: TextStyle(
-          color: Colors.white
-        ),
-        ),
+      floatingActionButton: Icon(Icons.add, color: Colors.white),
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: Get.height * 0.2,
+            right: Get.width * 0.1,
+            child: Icon(Icons.add, color: Colors.white),
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  SizedBox(height: Get.height*0.07,),
+                  Text('Community', style: GoogleFonts.manrope(color: Color(0xffEEEEF0),
+                  fontWeight: FontWeight.w700,
+                  fontSize: 22
+                  
+                  )),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

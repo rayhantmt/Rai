c6c7a800c0fc01c3b30452c0cd15fb9d8d8d6404
rayhantmt <rayhantmt@gmail.com>
2026-01-24 00:39:26 +0600

    added the back icon

--- lib/modules/create_new_group/create_new_group_view.dart
+++ lib/modules/create_new_group/create_new_group_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rai/utils/app_images.dart';
 
 class CreateNewGroupView extends StatelessWidget {
@@ -9,10 +11,28 @@ class CreateNewGroupView extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            
-          ],
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: Get.height*0.07,),
+              Row(
+                children: [
+                  Image.asset(AppImages.commentconcernbackicon,
+                  height: Get.height*0.05,
+                  ),
+                  SizedBox(width: Get.width*0.03,),
+                  Text('Create New Group',
+                  style: GoogleFonts.manrope( 
+                    fontWeight: FontWeight.w700,
+                    fontSize: 22,
+                    color: Color(0xffEEEEF0)
+                  ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );

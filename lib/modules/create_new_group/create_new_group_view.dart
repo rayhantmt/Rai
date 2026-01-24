884869143588aa37b248bda931ff8270c6da9f93
rayhantmt <rayhantmt@gmail.com>
2026-01-24 23:42:27 +0600

    added the section create group

--- lib/modules/create_new_group/create_new_group_view.dart
+++ lib/modules/create_new_group/create_new_group_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rai/common_widgets/common_button.dart';
+import 'package:rai/common_widgets/common_textfield.dart';
 import 'package:rai/utils/app_images.dart';
 
 class CreateNewGroupView extends StatelessWidget {
@@ -30,7 +32,55 @@ class CreateNewGroupView extends StatelessWidget {
                   ),
                   ),
                 ],
-              )
+              ),
+              SizedBox(height: Get.height*0.04,),
+              CircleAvatar(
+                radius: 80,
+                backgroundImage: NetworkImage('https://i.pravatar.cc/150?img=12'),
+              ),
+              SizedBox(height: Get.height*0.03,),
+              Text('Upload Group Photo',
+              style: GoogleFonts.manrope(
+                fontWeight: FontWeight.w700,
+                fontSize: 16,
+                color: Color(0xffEEEEF0)
+              ),
+              ),
+              SizedBox(height: Get.height*0.02,),
+              Divider(
+                height: 1,
+                endIndent: 1,
+                indent: 1,
+                color: Color(0xff393A40),
+              ),
+              Commontextfield(tittle: 'Group Name', hint: 'Write group name', obsecuretext: false),
+              SizedBox(
+                height: Get.height*0.01,
+              ),
+              Row(
+                children: [
+                  Text('Short Description',
+                  style: GoogleFonts.manrope( 
+                    fontWeight: FontWeight.w400,
+                    fontSize: 16,
+                    color: Color(0xffEEEEF0)
+                  ),
+                  ),
+                  
+                ],
+              ),
+              Container(
+                    width: double.infinity,
+                    height: Get.height*0.15,
+                    decoration: BoxDecoration(
+                     borderRadius: BorderRadius.circular(12),
+                      border: Border.all(
+                        width: 1,
+                        color: Color(0xff393A40)
+                      )
+                    ),
+                  ),
+                  CommonButton(tittle: 'Create Group')
             ],
           ),
         ),

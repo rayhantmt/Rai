657efb3589e08d8333989b7e27236c665293a213
rayhantmt <rayhantmt@gmail.com>
2026-01-28 06:24:13 +0600

    added the first name and last name section

--- lib/modules/edit_profile/edit_profile_view.dart
+++ lib/modules/edit_profile/edit_profile_view.dart
@@ -15,68 +15,157 @@ class EditProfileView extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              SizedBox(height: Get.height*0.07,),
+              SizedBox(height: Get.height * 0.07),
               Row(
                 children: [
                   GestureDetector(
                     onTap: () => Get.back(),
-                    child: Image.asset(AppImages.commentconcernbackicon,
-                    height: Get.height*0.05,
+                    child: Image.asset(
+                      AppImages.commentconcernbackicon,
+                      height: Get.height * 0.05,
                     ),
                   ),
-                  SizedBox(width: Get.width*0.05,),
-                  Text('Edit Profile',
-                  style: GoogleFonts.manrope( 
-                    fontWeight: FontWeight.w700,
-                    fontSize: 22,
-                    color: Color(0xffEEEEF0)
+                  SizedBox(width: Get.width * 0.05),
+                  Text(
+                    'Edit Profile',
+                    style: GoogleFonts.manrope(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 22,
+                      color: Color(0xffEEEEF0),
+                    ),
                   ),
-                  )
                 ],
               ),
-              SizedBox(height: Get.height*0.02,),
+              SizedBox(height: Get.height * 0.02),
               Container(
-                height: Get.height*0.15,
+                height: Get.height * 0.15,
                 child: Row(
                   children: [
                     CircleAvatar(
-                      backgroundImage: AssetImage(AppImages.profileimg,
-                      
-                      ),
-                      radius: Get.height*0.07,
+                      backgroundImage: AssetImage(AppImages.profileimg),
+                      radius: Get.height * 0.07,
                     ),
                     Column(
                       children: [
-                        Text('Must be JPG,JPEG or PNG',
-                        style: GoogleFonts.manrope( 
-                          fontWeight: FontWeight.w500,
-                          fontSize: 12,
-                          color: Color(0xffB2B3BD)
+                        Text(
+                          'Must be JPG,JPEG or PNG',
+                          style: GoogleFonts.manrope(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12,
+                            color: Color(0xffB2B3BD),
+                          ),
                         ),
-                        ),
-                          Text('64×64 px, under 2 MB',
-                        style: GoogleFonts.manrope( 
-                          fontWeight: FontWeight.w500,
-                          fontSize: 12,
-                          color: Color(0xffB2B3BD)
-                        ),
+                        Text(
+                          '64×64 px, under 2 MB',
+                          style: GoogleFonts.manrope(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12,
+                            color: Color(0xffB2B3BD),
+                          ),
                         ),
                         Spacer(),
-                        Text('UPload Image',
-                        style: GoogleFonts.manrope( 
-                          fontWeight: FontWeight.w700,
-                          fontSize: 16,
-                          color: Color(0xffEEEEF0)
+                        Text(
+                          'UPload Image',
+                          style: GoogleFonts.manrope(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16,
+                            color: Color(0xffEEEEF0),
+                          ),
                         ),
-                        ),
-                        SizedBox(
-                          height: Get.height*0.02,
-                        )
+                        SizedBox(height: Get.height * 0.02),
                       ],
-                    )
+                    ),
                   ],
                 ),
-              )
+              ),
+              Divider(
+                height: 1,
+                indent: 1,
+                endIndent: 1,
+                thickness: 1,
+                color: Color(0xff393A40),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'First Name',
+                        style: GoogleFonts.manrope(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                          color: Color(0xffEEEEF0),
+                        ),
+                      ),
+                      Container(
+                        height: Get.height * 0.05,
+                        width: Get.width * 0.4,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 1,
+                            color: Color(0xff393A40),
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.only(left: 10),
+                            border: InputBorder.none,
+                            hint: Text(
+                              'Enter first name',
+                              style: GoogleFonts.manrope(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Last Name',
+                        style: GoogleFonts.manrope(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 16,
+                          color: Color(0xffEEEEF0),
+                        ),
+                      ),
+                      Container(
+                        height: Get.height * 0.05,
+                        width: Get.width * 0.4,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            width: 1,
+                            color: Color(0xff393A40),
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.only(left: 10),
+                            border: InputBorder.none,
+                            hint: Text(
+                              'Enter last name',
+                              style: GoogleFonts.manrope(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ],
           ),
         ),
--- lib/modules/update_community/update_community_view.dart
+++ lib/modules/update_community/update_community_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:rai/common_widgets/common_button.dart';
+import 'package:rai/common_widgets/common_textfield.dart';
 import 'package:rai/utils/app_images.dart';
 
 class UpdateCommunityView extends StatelessWidget {
@@ -6,12 +10,122 @@ class UpdateCommunityView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          Scaffold(backgroundColor: AppImages.primarycolor),
-          Scaffold(backgroundColor: Colors.red),
-        ],
+    return Scaffold(
+      backgroundColor: AppImages.primarycolor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: Get.height * 0.07),
+              Row(
+                children: [
+                  GestureDetector(
+                    onTap: Get.back,
+                    child: Image.asset(
+                      AppImages.commentconcernbackicon,
+                      height: Get.height * 0.05,
+                    ),
+                  ),
+                  SizedBox(width: Get.width * 0.03),
+                  Text(
+                    'Update Group Info',
+                    style: GoogleFonts.manrope(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 22,
+                      color: Color(0xffEEEEF0),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: Get.height * 0.04),
+              CircleAvatar(
+                radius: 80,
+                backgroundImage: NetworkImage(
+                  'https://i.pravatar.cc/150?img=12',
+                ),
+              ),
+              SizedBox(height: Get.height * 0.03),
+              Text(
+                'Upload Group Photo',
+                style: GoogleFonts.manrope(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 16,
+                  color: Color(0xffEEEEF0),
+                ),
+              ),
+              SizedBox(height: Get.height * 0.02),
+              Divider(
+                height: 1,
+                endIndent: 1,
+                indent: 1,
+                color: Color(0xff393A40),
+              ),
+              Commontextfield(
+                tittle: 'Group Name',
+                hint: 'Write group name',
+                obsecuretext: false,
+              ),
+              SizedBox(height: Get.height * 0.01),
+              Row(
+                children: [
+                  Text(
+                    'Short Description',
+                    style: GoogleFonts.manrope(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16,
+                      color: Color(0xffEEEEF0),
+                    ),
+                  ),
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                height: Get.height * 0.15,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  border: Border.all(width: 1, color: Color(0xff393A40)),
+                ),
+                child: TextFormField(
+                  
+                  decoration: InputDecoration(
+                    
+                    border: InputBorder.none,
+                    hint: Text('Write short description here....',
+                    style: GoogleFonts.manrope( 
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      color: Color(0xff6C6E79)
+                    ),
+                    )
+                  ),
+                   style: GoogleFonts.inter(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14,
+                    color: Colors.white,
+                  ),
+                  
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Approval required to join',
+                    style: GoogleFonts.manrope(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 16,
+                      color: Color(0xffEEEEF0),
+                    ),
+                  ),
+                  Switch(value: true, onChanged: (value) => {}),
+                ],
+              ),
+              SizedBox(height: Get.height * 0.06),
+              CommonButton(tittle: 'Update'),
+            ],
+          ),
+        ),
       ),
     );
   }

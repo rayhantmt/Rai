afc5edc6be33b5fa74c30c3a9f6ba2fee3356cb2
rayhantmt <rayhantmt@gmail.com>
2026-02-03 05:52:21 +0600

    added the app bar

--- lib/modules/rais_picks/rais_picks_view.dart
+++ lib/modules/rais_picks/rais_picks_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:rai/utils/app_images.dart';
 
 class RaisPicksView extends StatelessWidget {
   const RaisPicksView({super.key});
@@ -6,8 +9,23 @@ class RaisPicksView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Raispicks'),
+      backgroundColor: AppImages.primarycolor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(height: Get.height*0.06,),
+              Text("rai's Picks",
+              style: GoogleFonts.manrope( 
+                fontWeight: FontWeight.w700,
+                fontSize: 22,
+                color: Color(0xffEEEEF0)
+              ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }

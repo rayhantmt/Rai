c5b1a1cb4cd065978aab0da6c1d7f4250988dc47
rayhantmt <rayhantmt@gmail.com>
2026-02-07 00:21:12 +0600

    added the game type and pick type section

---
+++ lib/modules/rais_picks/rais_picks_model.dart
@@ -0,0 +1,15 @@
+class RaisPicksModel {
+  final String gametype;
+
+  RaisPicksModel({required this.gametype});
+  
+
+  
+}
+
+class Picksmodel{
+  final String type;
+
+  Picksmodel({required this.type});
+
+}

56ae99653703be0d812e8830a45df1320913e51c
rayhantmt <rayhantmt@gmail.com>
2026-02-07 00:25:34 +0600

    added the games and picks types

--- lib/modules/rais_picks/rais_pics_controller.dart
+++ lib/modules/rais_picks/rais_pics_controller.dart
@@ -1,8 +1,22 @@
 import 'package:get/get.dart';
+import 'package:rai/modules/rais_picks/rais_picks_model.dart';
 
 class RaisPicsController extends GetxController{
   var selectedCategoryIndex=0.obs;
   void selectCategory(int index) {
     selectedCategoryIndex.value = index;
   }
+  var picks=[
+    Picksmodel(type: 'Bang for Buck'),
+    Picksmodel(type: 'Parlay Builder'),
+    Picksmodel(type: 'Daily Picks'),
+    Picksmodel(type: 'Pick of the Day'),
+  ];
+  var games=[
+    RaisPicksModel(gametype: 'Basketball'),
+    RaisPicksModel(gametype: 'Football'),
+    RaisPicksModel(gametype: 'Hockey'),
+    RaisPicksModel(gametype: 'Tenis'),
+    RaisPicksModel(gametype: 'Cricket')
+  ];
 }

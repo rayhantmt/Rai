3889e8dd1dec97959f39e044398727eebcc614ea
rayhantmt <rayhantmt@gmail.com>
2026-02-10 20:32:10 +0600

    added the upper section

--- lib/modules/main_screen/main_screen_binding.dart
+++ lib/modules/main_screen/main_screen_binding.dart
@@ -1,11 +1,13 @@
 import 'package:get/get.dart';
 import 'package:rai/modules/community/community_controller.dart';
 import 'package:rai/modules/main_screen/main_screen_controller.dart';
+import 'package:rai/modules/rais_picks/rais_pics_controller.dart';
 
 class MainScreenBinding extends Bindings{
   @override
   void dependencies() {
     Get.lazyPut<MainScreenController>(() => MainScreenController(),);
     Get.lazyPut<CommunityController>(() => CommunityController());
+    Get.lazyPut<RaisPicsController>(() => RaisPicsController(),);
   }
 }
--- lib/modules/rais_picks/rais_picks_view.dart
+++ lib/modules/rais_picks/rais_picks_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:rai/modules/rais_picks/rais_pics_controller.dart';
 import 'package:rai/utils/app_images.dart';
 
-class RaisPicksView extends StatelessWidget {
+class RaisPicksView extends GetView<RaisPicsController> {
   const RaisPicksView({super.key});
 
   @override
@@ -15,13 +16,47 @@ class RaisPicksView extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             children: [
-              SizedBox(height: Get.height*0.06,),
-              Text("rai's Picks",
-              style: GoogleFonts.manrope( 
-                fontWeight: FontWeight.w700,
-                fontSize: 22,
-                color: Color(0xffEEEEF0)
+              SizedBox(height: Get.height * 0.06),
+              Text(
+                "rai's Picks",
+                style: GoogleFonts.manrope(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 22,
+                  color: Color(0xffEEEEF0),
+                ),
               ),
+              SizedBox(height: Get.height * 0.02),
+              SizedBox(
+                height: Get.height * 0.2,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: controller.picks.length,
+                  itemBuilder: (context, index) => SizedBox(
+                    height: Get.height * 0.1,
+                    child: Container(
+                      width: Get.width * 0.3,
+                      child: Column(
+                        children: [
+                          Text(
+                            controller.picks[index].type,
+                            style: GoogleFonts.manrope(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 14,
+                              color: Color(0xffEEEEF0),
+                            ),
+                          ),
+                          Divider(
+                            height: 1,
+                            indent: 5,
+                            endIndent: 5,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
               ),
             ],
           ),

361f19c8034fb1eac2fe6ce1ebace4742e0ceabd
rayhantmt <rayhantmt@gmail.com>
2026-02-11 02:27:11 +0600

    giving the final otuch

--- lib/modules/rais_picks/rais_picks_view.dart
+++ lib/modules/rais_picks/rais_picks_view.dart
@@ -11,108 +11,114 @@ class RaisPicksView extends GetView<RaisPicsController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: Get.height * 0.06),
-              Text(
-                "rai's Picks",
-                style: GoogleFonts.manrope(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 22,
-                  color: Color(0xffEEEEF0),
-                ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: Get.height * 0.06),
+            Text(
+              "rai's Picks",
+              style: GoogleFonts.manrope(
+                fontWeight: FontWeight.w700,
+                fontSize: 22,
+                color: Color(0xffEEEEF0),
               ),
-              SizedBox(height: Get.height * 0.02),
-              SizedBox(
-                height: Get.height * 0.05,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: controller.picks.length,
-                  itemBuilder: (context, index) => SizedBox(
-                    height: Get.height * 0.1,
-                    child: GestureDetector(
-                      onTap: () => controller.selectpicks(index),
-                      child: Container(
-                        width: Get.width * 0.3,
-                        child: Obx(
-                          () => Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Text(
-                                controller.picks[index].type,
-                                style: GoogleFonts.manrope(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 14,
-                                  color: controller.picksindex.value == index
-                                      ? Color(0xffEEEEF0)
-                                      : Color(0xff6C6E79),
-                                ),
-                              ),
-                              SizedBox(height: 4),
-                              Container(
-                                height: 1,
-
+            ),
+            SizedBox(height: Get.height * 0.02),
+            SizedBox(
+              height: Get.height * 0.05,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                itemCount: controller.picks.length,
+                itemBuilder: (context, index) => SizedBox(
+                  height: Get.height * 0.1,
+                  child: GestureDetector(
+                    onTap: () => controller.selectpicks(index),
+                    child: Container(
+                      width: Get.width * 0.3,
+                      child: Obx(
+                        () => Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Text(
+                              controller.picks[index].type,
+                              style: GoogleFonts.manrope(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 14,
                                 color: controller.picksindex.value == index
                                     ? Color(0xffEEEEF0)
                                     : Color(0xff6C6E79),
                               ),
-                            ],
-                          ),
+                            ),
+                            SizedBox(height: 4),
+                            Container(
+                              height: 1,
+      
+                              color: controller.picksindex.value == index
+                                  ? Color(0xffEEEEF0)
+                                  : Color(0xff6C6E79),
+                            ),
+                          ],
                         ),
                       ),
                     ),
                   ),
                 ),
               ),
-              SizedBox(
-                height: Get.height * 0.045,
-
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: controller.games.length,
-                  itemBuilder: (context, index) => GestureDetector(
-                    onTap: () => controller.selectgames(index),
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 8),
-                      child: Obx(() => Container(
-                        width: Get.width * 0.25,
-
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            width: 1,
-                             color: controller.gamesindex.value == index
-                              ? Color(0xffEEEEF0)
-                              : Color(0xff393A40),
-                          ),
-                          borderRadius: BorderRadius.circular(100),
-                          color: controller.gamesindex.value == index
-                              ? Color(0xffEEEEF0)
-                              : AppImages.primarycolor,
+            ),
+            SizedBox(
+              height: Get.height * 0.045,
+      
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: controller.games.length,
+                itemBuilder: (context, index) => GestureDetector(
+                  onTap: () => controller.selectgames(index),
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child: Obx(() => Container(
+                      width: Get.width * 0.25,
+      
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          width: 1,
+                           color: controller.gamesindex.value == index
+                            ? Color(0xffEEEEF0)
+                            : Color(0xff393A40),
                         ),
-                        child: Center(
-                          child: Text(
-                            controller.games[index].gametype,
-                            style: GoogleFonts.manrope(
-                              fontWeight: FontWeight.w700,
-                              fontSize: 14,
-                              color: controller.gamesindex.value == index
-                              ? Color(0xff19191B)
-                              : Color(0xff797B86),
-                            ),
+                        borderRadius: BorderRadius.circular(100),
+                        color: controller.gamesindex.value == index
+                            ? Color(0xffEEEEF0)
+                            : AppImages.primarycolor,
+                      ),
+                      child: Center(
+                        child: Text(
+                          controller.games[index].gametype,
+                          style: GoogleFonts.manrope(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                            color: controller.gamesindex.value == index
+                            ? Color(0xff19191B)
+                            : Color(0xff797B86),
                           ),
                         ),
-                      ),)
-                    ),
+                      ),
+                    ),)
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: controller.predictions.length,
+                shrinkWrap: true,
+                itemBuilder: (context, index) => Container(
+              height: Get.height*0.2,
+              color: Colors.red,
+            ),))
+          ],
         ),
       ),
     );
--- lib/modules/rais_picks/rais_pics_controller.dart
+++ lib/modules/rais_picks/rais_pics_controller.dart
@@ -1,35 +1,116 @@
 import 'package:get/get.dart';
 import 'package:rai/modules/rais_picks/rais_picks_model.dart';
 
-class RaisPicsController extends GetxController{
-  var selectedCategoryIndex=0.obs;
+class RaisPicsController extends GetxController {
+  var selectedCategoryIndex = 0.obs;
   void selectCategory(int index) {
     selectedCategoryIndex.value = index;
   }
-  var picks=[
+
+  var picks = [
     Picksmodel(type: 'Bang for Buck'),
     Picksmodel(type: 'Parlay Builder'),
     Picksmodel(type: 'Daily Picks'),
     Picksmodel(type: 'Pick of the Day'),
   ];
-  var games=[
+  var games = [
     RaisPicksModel(gametype: 'Basketball'),
     RaisPicksModel(gametype: 'Football'),
     RaisPicksModel(gametype: 'Hockey'),
     RaisPicksModel(gametype: 'Tenis'),
-    RaisPicksModel(gametype: 'Cricket')
+    RaisPicksModel(gametype: 'Cricket'),
   ];
-  RxInt picksindex=0.obs;
-  RxInt gamesindex=0.obs;
-  void selectpicks(int index){
-      print(picksindex.value);
+  RxInt picksindex = 0.obs;
+  RxInt gamesindex = 0.obs;
+  void selectpicks(int index) {
+    print(picksindex.value);
     print('list inedx $index');
-    picksindex.value=index;
+    picksindex.value = index;
   }
-  void selectgames(int index){
-    gamesindex.value=index;
+
+  void selectgames(int index) {
+    gamesindex.value = index;
     print(gamesindex.value);
     print('list inedx $index');
   }
-  
+
+  var predictions = [
+    Leaugemodel(
+      odds: '153+',
+      edge: "+8.23%",
+      ev: '+9.83%',
+      confidence: '82%',
+      point: '+5.5',
+    ),
+     Leaugemodel(
+      odds: '153+',
+      edge: "+8.23%",
+      ev: '+9.83%',
+      confidence: '82%',
+      point: '+5.5',
+    ),
+     Leaugemodel(
+      odds: '153+',
+      edge: "+8.23%",
+      ev: '+9.83%',
+      confidence: '82%',
+      point: '+5.5',
+    ),
+     Leaugemodel(
+      odds: '153+',
+      edge: "+8.23%",
+      ev: '+9.83%',
+      confidence: '82%',
+      point: '+5.5',
+    ),
+     Leaugemodel(
+      odds: '153+',
+      edge: "+8.23%",
+      ev: '+9.83%',
+      confidence: '82%',
+      point: '+5.5',
+    ),
+     Leaugemodel(
+      odds: '153+',
+      edge: "+8.23%",
+      ev: '+9.83%',
+      confidence: '82%',
+      point: '+5.5',
+    ),
+     Leaugemodel(
+      odds: '153+',
+      edge: "+8.23%",
+      ev: '+9.83%',
+      confidence: '82%',
+      point: '+5.5',
+    ),
+     Leaugemodel(
+      odds: '153+',
+      edge: "+8.23%",
+      ev: '+9.83%',
+      confidence: '82%',
+      point: '+5.5',
+    ),
+     Leaugemodel(
+      odds: '153+',
+      edge: "+8.23%",
+      ev: '+9.83%',
+      confidence: '82%',
+      point: '+5.5',
+    ),
+     Leaugemodel(
+      odds: '153+',
+      edge: "+8.23%",
+      ev: '+9.83%',
+      confidence: '82%',
+      point: '+5.5',
+    ),
+     Leaugemodel(
+      odds: '153+',
+      edge: "+8.23%",
+      ev: '+9.83%',
+      confidence: '82%',
+      point: '+5.5',
+    ),
+  ];
 }

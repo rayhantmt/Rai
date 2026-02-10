31ee1d945fd1cec06be8ec7cdd2057194bdfac00
rayhantmt <rayhantmt@gmail.com>
2026-02-11 02:19:48 +0600

    created the leauge model

--- lib/modules/rais_picks/rais_picks_model.dart
+++ lib/modules/rais_picks/rais_picks_model.dart
@@ -2,14 +2,26 @@ class RaisPicksModel {
   final String gametype;
 
   RaisPicksModel({required this.gametype});
-  
-
-  
 }
 
-class Picksmodel{
+class Picksmodel {
   final String type;
 
   Picksmodel({required this.type});
+}
 
+class Leaugemodel {
+  final String odds;
+  final String edge;
+  final String ev;
+  final String confidence;
+  final String point;
+
+  Leaugemodel({
+    required this.odds,
+    required this.edge,
+    required this.ev,
+    required this.confidence,
+    required this.point,
+  });
 }

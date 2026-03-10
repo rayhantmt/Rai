e6678a2faa941de7d9ea9d803da3bca8ae185c27
rayhantmt <rayhantmt@gmail.com>
2026-03-11 05:58:10 +0600

    done for today

--- lib/modules/fotgot_password/verificatio_page/verification_page_contorller.dart
+++ lib/modules/fotgot_password/verificatio_page/verification_page_contorller.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:rai/api_services/api_config.dart';
 import 'package:rai/api_services/api_services.dart';
 import 'package:rai/exceptions/app_exceptions.dart';
@@ -15,23 +14,20 @@ class VerificationPageContorller extends GetxController {
   final emailcontroller = TextEditingController();
   RxBool isLoading = false.obs;
   Future<void> requestOtp() async {
-    final storage = GetStorage();
     isLoading.value = true;
     final body = {"identifier": emailcontroller.text};
     try {
-      final token = storage.read('token');
+     
       final response2 = await ApiService.post(
         body: body,
         endpoint: ApiConfig.forgotpassotprequest,
-       // headers: {'Authorization': 'Bearer $token'},
+        // headers: {'Authorization': 'Bearer $token'},
       );
       print(response2);
-if (response2.statusCode == 200 || response2.statusCode == 201) {
-         Get.snackbar('Success', response2['message']);
+      if (response2.statusCode == 200 || response2.statusCode == 201) {
+        Get.snackbar('Success', response2['message']);
         Get.toNamed(AppPages.emailotpfp);
       }
-
-      
     } on AppException catch (e) {
       Get.snackbar("Otp Request Failed", e.message);
     } finally {

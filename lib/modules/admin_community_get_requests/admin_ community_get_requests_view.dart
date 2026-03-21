3732a8e018dc70e90a3c81103d01be82028b9b6a
rayhantmt <rayhantmt@gmail.com>
2026-03-21 13:15:04 +0600

    added the scaffold and background colour

--- lib/modules/admin_community_get_requests/admin_ community_get_requests_view.dart
+++ lib/modules/admin_community_get_requests/admin_ community_get_requests_view.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:rai/utils/app_images.dart';
 
 class AdmincommunityGetRequestsView extends StatelessWidget {
   const AdmincommunityGetRequestsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppImages.primarycolor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: Get.height*0.06,),
+            Row(
+              children: [
+                Image.asset(AppImages.commentconcernbackicon,height: Get.height*0.5,),
+                Text('Member List',
+                style: GoogleFonts.manrope(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 22,
+                  color: Color(0xffEEEEF0)
+                ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+
   }
 }

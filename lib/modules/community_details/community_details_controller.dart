33328941cf152ec68406bad8b4d12e74984eadc3
rayhantmt <rayhantmt@gmail.com>
2026-04-05 03:35:41 +0600

    done with the member adding

--- lib/modules/community_details/community_details_controller.dart
+++ lib/modules/community_details/community_details_controller.dart
@@ -1,7 +1,6 @@
 
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:rai/api_services/api_config.dart';
 import 'package:rai/api_services/api_services.dart';
 import 'package:rai/exceptions/app_exceptions.dart';
 

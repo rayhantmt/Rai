87b4bdf25a947d7a6f5ec60b5745cda3bbbb91a3
rayhantmt <rayhantmt@gmail.com>
2026-04-05 03:29:58 +0600

    done with the community section

--- lib/modules/community_chat_details/community_chat_details_controller.dart
+++ lib/modules/community_chat_details/community_chat_details_controller.dart
@@ -1,49 +1,3 @@
-// import 'package:get/get.dart';
-// import 'package:get_storage/get_storage.dart';
-// import 'package:rai/api_services/api_services.dart';
-// import 'package:rai/exceptions/app_exceptions.dart';
-// import 'package:rai/modules/community_chat_details/community_chat_details_model.dart';
-
-// class CommunityChatDetailsController extends GetxController {
-//   var id = ''.obs;
-//   var name = ''.obs;
-//   var img = ''.obs;
-//   var member = ''.obs;
-
-//   @override
-//   void onInit() {
-//     super.onInit();
-
-//     final args = Get.arguments;
-
-//     id.value = args['id'];
-//     fetchdetails();
-//   }
-
-//   RxBool isLoading = false.obs;
-//   Rxn<CommunityChatDetailsModel> communityDetails = Rxn<CommunityChatDetailsModel>();
-//   Future<void> fetchdetails() async {
-//     isLoading.value = true;
-//     try {
-//       final storage = GetStorage();
-//       final token = storage.read('token');
-//       final response2 = await ApiService.get(
-//         endpoint: '/api/community/${id.value}/',
-//         headers: {'Authorization': 'Bearer $token'},
-//       );
-//       print(response2);
-//        communityDetails.value = CommunityChatDetailsModel.fromJson(response2['data']);
-//       final data = response2['data'];
-//       print(data);
-//     } on AppException catch (e) {
-//       print(e.message);
-//       Get.snackbar("Error", e.message);
-//     } finally {
-//       isLoading.value = false;
-//     }
-//   }
-// }
-
 import 'dart:async';
 import 'dart:convert';
 
@@ -55,8 +9,6 @@ import 'package:rai/modules/rai_chat/rai_chat_controller.dart' hide ChatMessage;
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:rai/api_services/api_services.dart';
 import 'package:rai/exceptions/app_exceptions.dart';
-
-
 
 const String _wsBaseUrl =  'wss://quicker-epistylar-barbie.ngrok-free.dev';
 
@@ -71,7 +23,7 @@ class CommunityChatDetailsController extends GetxController {
   Rxn<CommunityChatDetailsModel> communityDetails =
       Rxn<CommunityChatDetailsModel>();
 
-  // ── Chat / WebSocket state ──────────────────────────────────────────────────
+  // ── Chat / WebSocket state 
   final RxList<ChatMessage> messages = <ChatMessage>[].obs;
   final RxBool isConnected = false.obs;
   final RxBool isConnecting = false.obs;

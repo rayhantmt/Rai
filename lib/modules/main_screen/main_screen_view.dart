b58f757a7b3ac71793a6c867556ad9df629ffe2a
rayhantmt <rayhantmt@gmail.com>
2026-04-12 00:21:55 +0600

    fixed the bottom bar

--- lib/modules/main_screen/main_screen_view.dart
+++ lib/modules/main_screen/main_screen_view.dart
@@ -51,123 +51,140 @@ class MainScreenView extends GetView<MainScreenController> {
                   children: [
                     GestureDetector(
                       onTap: () => controller.setpagetype(0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            AppImages.communityicon,
-                            height: Get.height * 0.035,
-                            color: controller.pagetype.value == 0
-                                ? Color(0xffEEEEF0)
-                                : Color(0xff6C6E79),
-                          ),
-                          Text(
-                            'Community',
-                            style: GoogleFonts.manrope(
-                              fontWeight: FontWeight.w500,
+                      child: Container(
+                        width: Get.width*0.21,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              AppImages.communityicon,
+                              height: Get.height * 0.035,
                               color: controller.pagetype.value == 0
                                   ? Color(0xffEEEEF0)
                                   : Color(0xff6C6E79),
                             ),
-                          ),
-                        ],
+                            Text(
+                              'Community',
+                              maxLines: 1,
+                              style: GoogleFonts.manrope(
+                                fontWeight: FontWeight.w500,
+                                color: controller.pagetype.value == 0
+                                    ? Color(0xffEEEEF0)
+                                    : Color(0xff6C6E79),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                     GestureDetector(
                       onTap: () => controller.setpagetype(1),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            AppImages.aiicon,
-
-                            height: Get.height * 0.035,
-                            color: controller.pagetype.value == 1
-                                ? Color(0xffEEEEF0)
-                                : Color(0xff6C6E79),
-                          ),
-                          Text(
-                            'rai. Chat',
-                            style: GoogleFonts.manrope(
-                              fontWeight: FontWeight.w500,
+                      child: Container(
+                        width: Get.width*0.15,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              AppImages.aiicon,
+                              height: Get.height * 0.035,
                               color: controller.pagetype.value == 1
                                   ? Color(0xffEEEEF0)
                                   : Color(0xff6C6E79),
                             ),
-                          ),
-                        ],
+                            Text(
+                              'rai. Chat',
+                              style: GoogleFonts.manrope(
+                                fontWeight: FontWeight.w500,
+                                color: controller.pagetype.value == 1
+                                    ? Color(0xffEEEEF0)
+                                    : Color(0xff6C6E79),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                     GestureDetector(
                       onTap: () => controller.setpagetype(2),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            AppImages.homeicon,
-                            height: Get.height * 0.035,
-                            color: controller.pagetype.value == 2
-                                ? Color(0xffEEEEF0)
-                                : Color(0xff6C6E79),
-                          ),
-                          Text(
-                            'Home',
-                            style: GoogleFonts.manrope(
-                              fontWeight: FontWeight.w500,
+                      child: Container(
+                        width: Get.width*0.17,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              AppImages.homeicon,
+                              height: Get.height * 0.035,
                               color: controller.pagetype.value == 2
                                   ? Color(0xffEEEEF0)
                                   : Color(0xff6C6E79),
                             ),
-                          ),
-                        ],
+                            Text(
+                              'Home',
+                              style: GoogleFonts.manrope(
+                                fontWeight: FontWeight.w500,
+                                color: controller.pagetype.value == 2
+                                    ? Color(0xffEEEEF0)
+                                    : Color(0xff6C6E79),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                     GestureDetector(
                       onTap: () => controller.setpagetype(3),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            AppImages.picksicon,
-                            height: Get.height * 0.035,
-                            color: controller.pagetype.value == 3
-                                ? Color(0xffEEEEF0)
-                                : Color(0xff6C6E79),
-                          ),
-                          Text(
-                            "rai's picks",
-                            style: GoogleFonts.manrope(
-                              fontWeight: FontWeight.w500,
+                      child: Container(
+                         width: Get.width*0.17,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              AppImages.picksicon,
+                              height: Get.height * 0.035,
                               color: controller.pagetype.value == 3
                                   ? Color(0xffEEEEF0)
                                   : Color(0xff6C6E79),
                             ),
-                          ),
-                        ],
+                            Text(
+                              "rai's picks",
+                              maxLines: 1,
+                              style: GoogleFonts.manrope(
+                                
+                                fontWeight: FontWeight.w500,
+                                color: controller.pagetype.value == 3
+                                    ? Color(0xffEEEEF0)
+                                    : Color(0xff6C6E79),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                     GestureDetector(
                       onTap: () => controller.setpagetype(4),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            AppImages.moreicon,
-                            height: Get.height * 0.035,
-                            color: controller.pagetype.value == 4
-                                ? Color(0xffEEEEF0)
-                                : Color(0xff6C6E79),
-                          ),
-                          Text(
-                            'More',
-                            style: GoogleFonts.manrope(
-                              fontWeight: FontWeight.w500,
+                      child: Container(
+                        width: Get.width*0.17,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              AppImages.moreicon,
+                              height: Get.height * 0.035,
                               color: controller.pagetype.value == 4
                                   ? Color(0xffEEEEF0)
                                   : Color(0xff6C6E79),
                             ),
-                          ),
-                        ],
+                            Text(
+                              'More',
+                              style: GoogleFonts.manrope(
+                                fontWeight: FontWeight.w500,
+                                color: controller.pagetype.value == 4
+                                    ? Color(0xffEEEEF0)
+                                    : Color(0xff6C6E79),
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],

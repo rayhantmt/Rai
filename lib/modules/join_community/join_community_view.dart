48802544943b1a496201272436aaf573ebb1084f
rayhantmt <rayhantmt@gmail.com>
2026-04-12 04:52:18 +0600

    done with the sectio

--- lib/modules/join_community/join_community_view.dart
+++ lib/modules/join_community/join_community_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/state_manager.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:rai/modules/join_community/join_community_controller.dart';
 import 'package:rai/utils/app_images.dart';
 
@@ -10,6 +12,23 @@ class JoinCommunityView extends GetView<JoinCommunityController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppImages.primarycolor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          children: [
+            SizedBox(height: Get.height * 0.3),
+            Text(
+              'Enter code to join that community',
+        
+              style: GoogleFonts.manrope(
+                fontWeight: FontWeight.w700,
+                fontSize: 16,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }

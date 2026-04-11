3acc20feac7ddae848dee7e34ae28a0af548f342
rayhantmt <rayhantmt@gmail.com>
2026-04-12 04:30:29 +0600

    added the background color

--- lib/modules/join_community/join_community_view.dart
+++ lib/modules/join_community/join_community_view.dart
@@ -1,12 +1,15 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 import 'package:get/state_manager.dart';
 import 'package:rai/modules/join_community/join_community_controller.dart';
+import 'package:rai/utils/app_images.dart';
 
 class JoinCommunityView extends GetView<JoinCommunityController> {
   const JoinCommunityView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppImages.primarycolor,
+    );
   }
 }
